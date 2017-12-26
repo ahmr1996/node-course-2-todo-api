1d6e5db3bc9c18bd575f2099496674260651df43
Alexis <alexismunoz_1996@hotmail.com>
2017-12-26 06:13:20 -0300

    Add POST /users route and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+//mongoose.set('debug', true);
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose }
